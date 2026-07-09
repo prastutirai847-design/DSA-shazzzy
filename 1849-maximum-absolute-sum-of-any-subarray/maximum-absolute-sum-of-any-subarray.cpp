08d8955ea58611ba99abb7a0c1bf2c009c0defeb
Prastuti <prastutirai847@gmail.com>
2026-07-09 21:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (88.59%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
-        int bestend=nums[0],worstend=nums[0];int ans=nums[0];
-       for (int i = 1; i <nums.size();  i ++ ){
-        int v1=bestend+nums[i];
-        int v2=worstend+nums[i];
-        int v3=nums[i];
-         bestend=max(v3,v1);
-        worstend=min(v3,v2);
-       
-        ans=max(ans,max(   abs(worstend),abs(bestend)));
-       
-       } 
-       if(ans<0)
-       return abs(ans);
-       else
-       return ans; 
-       
+        int ans=abs(nums[0]),worstend=nums[0],bestend=nums[0];
+        for (int i = 1; i < nums.size(); i++){
+            int v1=bestend+nums[i];
+            int v2=worstend+nums[i];
+            int v3=nums[i];
+            bestend=max(v3,max(v2,v1));
+            worstend=min(v3,min(v2,v1));
+            ans=max(abs(ans),max(abs(bestend),abs(worstend)));
+        }
+        
+        return ans;
+        
     }
 };
