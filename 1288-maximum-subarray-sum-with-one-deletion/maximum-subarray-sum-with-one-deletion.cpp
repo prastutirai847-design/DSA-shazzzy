0daf6477308441c5e4218a4047131be1135c5e92
Prastuti <prastutirai847@gmail.com>
2026-07-10 18:30:20 +0530

    Time: 0 ms (100.00%) | Memory: 27.3 MB (21.84%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        int ans=arr[0];int nodlt = arr[0];
-int onedlt = 0;
-        for(int i = 1; i < arr.size(); i++){
-        int prevonedelt=onedlt;
-        int pervnodelt=nodlt;
-        nodlt=max(pervnodelt+arr[i],arr[i]);
-        onedlt=max(prevonedelt+arr[i],pervnodelt);
-        ans=max(ans,max(nodlt,onedlt));
-
+        int onedelete =0,nodelete=arr[0],ans=arr[0];
+        
+        for (int i = 1; i < arr.size(); i++){
+            int prevonedelt=onedelete , prevnodelt=nodelete ;
+            onedelete=max(prevonedelt+arr[i],prevnodelt);//in this case we will delete only one node. We haven't done it yet but it's possible that we will delete one node in this case. Either we will do it before or after deleting the previous node or we have to delete the previous one now. If we want to delete the previous node then we will delete the previous one and keep to see what is the maximum of this one to delete 
+            nodelete=max(prevnodelt+arr[i],arr[i]);
+            ans=max(ans,max(onedelete,nodelete));
 
         }
         return ans;
-
-        
     }
 };
